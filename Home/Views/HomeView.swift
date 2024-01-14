5d35684247a3801e4735bc17e46513ccc19ac06a
Russell Gordon <rgordon@lcs.on.ca>
2024-01-14 14:58:59 -0500

    Re-organized project files.

---
+++ Home/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  Home
+//
+//  Created by Russell Gordon on 2024-01-14.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
